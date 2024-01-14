13d2a71af8819cf400b1fda96157b8207196a3f9
tevhidehli <156154383+tevhidehli@users.noreply.github.com>
2024-01-14 04:33:58 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,23 @@ const initApp = () => {
     mobileMenu.addEventListener('click', toggleMenu)
 }
 
-document.addEventListener('DOMContentLoaded', initApp)
+document.addEventListener('DOMContentLoaded', initApp);
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+  
+  
